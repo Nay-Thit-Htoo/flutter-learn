19b944b73d81836925e60fa53a1d7ff582809061
Nay Thit Htoo <68145588+Nay-Thit-Htoo@users.noreply.github.com>
2020-07-25 17:59:44 +0630

    threedialogbutton
    Dialog Buttonမှာ Button သုံးခုထည့်ပြီးစမ်းကြည့်မယ်

---
+++ threebuttondialogs.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "TextField and Button",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Text Flutter"),
+        ),
+        body: MyWidget(),
+      ),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  _MywidgetState createState() => _MywidgetState();
+}
+
+class _MywidgetState extends State<MyWidget> {
+  bool _checkvalue = false;
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: Text('Show Alert Box'),
+      onPressed: () {
+        //called alert dialog box function
+        _doSomething();
+      },
+    );
+  }
+
+  void _doSomething() {
+    //for ok button for alert dialog box
+    Widget okButton = FlatButton(
+      child: Text("Ok"),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+
+    //for cancel button for alert dialog box
+    Widget cancelButton = FlatButton(
+      child: Text("Cancel"),
+      onPressed: () {},
+    );
+
+    //for exit button for alert dialog button
+    Widget exitButton = FlatButton(
+      child: Text("Exit"),
+      onPressed: () {},
+    );
+
+    //create alert dialog box
+    AlertDialog alert = AlertDialog(
+      title: Text("Dialog Title"),
+      content: Text("This is a flutter AlertDialog"),
+      actions: [okButton, cancelButton, exitButton],
+    );
+
+    //it is default function for alert dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+}
